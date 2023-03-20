22ba49a3d18f014f1a50e2a76eb0f8fc3f80c833
Oblivionburn <donney.lane@gmail.com>
2023-03-20 05:57:10 -0500

    v1.2
    Extracted network/neuron classes to a separate NetworkModule project

---
+++ MemoryModule/MemoryManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using NetworkModule;
+
+namespace MemoryModule
+{
+    public static class MemoryManager
+    {
+        public static List<Memory> Memories = new List<Memory>();
+
+        public static void ReceiveData(List<Neuron> neurons)
+        {
+            //Add new frame of data to memory for later recollection
+            Memory memory = CurrentMemory();
+            memory.AddFrame(new Frame(neurons));
+
+            //If memory has grown stale from receiving repeating data, then start a new memory
+            if (memory.Staleness >= 1)
+            {
+                Memories.Add(new Memory());
+            }
+        }
+
+        public static Memory CurrentMemory()
+        {
+            Memory memory = null;
+            if (Memories.Count == 0)
+            {
+                memory = new Memory();
+                Memories.Add(memory);
+            }
+            else
+            {
+                int count = Memories.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    Memory existing = Memories[i];
+                    if (i == count - 1)
+                    {
+                        //Return most recent memory
+                        memory = existing;
+                    }
+                    else
+                    {
+                        //Fade old memories
+                        existing.Decay();
+                    }
+                }
+            }
+
+            return memory;
+        }
+
+        public static List<Memory> RecallMemories(List<DataPacket> dataPackets)
+        {
+            List<Memory> memories = new List<Memory>();
+
+            float similar = 0;
+
+            int dataCount = dataPackets.Count;
+            int memCount = Memories.Count;
+
+            for (int m = 0; m < memCount; m++)
+            {
+                Memory memory = Memories[m];
+
+                for (int d = 0; d < dataCount; d++)
+                {
+                    DataPacket packet = dataPackets[d];
+                    if (memory.HasData(packet))
+                    {
+                        similar++;
+                    }
+                }
+
+                float percent_similar = (similar * 100) / dataCount;
+                if (percent_similar >= 80)
+                {
+                    //Increase memory strength from recollection
+                    memory.Weight++;
+
+                    memories.Add(memory);
+                }
+                else
+                {
+                    //Decay old memories
+                    memory.Decay();
+                }
+            }
+
+            return memories;
+        }
+    }
+}
